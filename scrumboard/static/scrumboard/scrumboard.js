f6cac70d70b4547f3edcbfb053bc4ffb12051d08
Cristian <cristianalemanfuentes@gmail.com>
2017-06-25 18:39:05 -0500

    Succesfully POST using .post

--- scrumboard/static/scrumboard/scrumboard.js
+++ scrumboard/static/scrumboard/scrumboard.js
@@ -13,15 +13,22 @@ For testing:
         function ScrumboardController($scope,$http){
             $scope.add = function(list,title){
                 var card = {
+                    list : list.id,
                     title : title
                 };
-                $http.post('/scrumboard/cards/',card);
-                list.cards.push(card);
+                $http.post('/scrumboard/cards/',card)
+                    .then(function(response){
+                       list.cards.push(response.data);
+                    },
+                    function(){
+                        alert('Could not create card');
+                    });
             };
             
             $scope.data = [];
-            $http.get('/scrumboard/lists/').then(function(response){
-                $scope.data = response.data;
+            $http.get('/scrumboard/lists/').then(
+                function(response){
+                    $scope.data = response.data;
             });
         }
 
